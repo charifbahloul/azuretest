4399a71fd8360e93da99820dfb06ee8058a02638
Charif Bahloul <shariefbahloul@gmail.com>
2023-02-25 20:02:11 -0500

    Added files to test

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
+@app.route("/summary", methods=["GET"])
+def get_summary():
+    # return "1\n2\n3"
+    with open("files/summary.txt", "r", encoding="utf-8") as f:
+        return f.readlines()
+
 if __name__ == '__main__':
    app.run()
